0a2ff07dc1c6ebae842f369c4fa0fe2f27d0f0a8
Ibrahim Diabate <diabai@uw.edu>
2017-11-30 20:44:24 -0800

    cleaned up

--- main.py
+++ main.py
@@ -5,42 +5,43 @@ from numpy import matrix as Matrix
 
 def main():
     np.seterr(divide="ignore", invalid="ignore")
-
     adjacencyMatrix = np.genfromtxt('graph.txt', dtype=np.int64, delimiter=" ")
-    print("Adjacency Matrix")
+
+    print("Adjacency Matrix from file")
     print(adjacencyMatrix)
 
     #Finds the largest index in both columns
     largestIndex = adjacencyMatrix[:,0:2].max()
     n = largestIndex+1
-    # Matrix of size of file's matrix
+    # Creates matrix of size of file's matrix
     mMatrix = np.zeros((n, n))
     for row in adjacencyMatrix:
         i, j, k= row
         mMatrix[j, i] = k
 
-    print("mMatrix")
+    print("Connection Matrix")
     print(mMatrix)
     beta = 0.85
-    # Gets column sum
+    # Gets columns sums
     sumVector = mMatrix.sum(0)
-    #print(sumVector)
     #Replace all 0s with 1
     sumVector[sumVector == 0] = 1
-    #print(sumVector)
 
+    # Generates the initial stochastic matrix
     stochasticArray = mMatrix / sumVector
+
+    print("Stochastic matrix: ")
+    print(stochasticArray)
 
     # Beta * stochastic array
     betaTimesStoch = stochasticArray * beta
-    # print(betaTimesStoch)
 
-     # Size of a column
+    # Size of a column
     n = mMatrix.shape[0]
     #Leap probabilty
     leapProp = (1-beta)/n
 
-    # Repeat creates a list of 1 of size n  ; .transpose turn matrix to vertical
+    # Repeat creates a list of 1 of size n; .transpose turn matrix to vertical
     # Original rank vector
     r = Matrix(np.repeat(np.divide(1,n), n)).transpose()
     print("Original rank vector:")
@@ -49,8 +50,7 @@ def main():
     prevR = np.zeros(r.shape)
 
     iterations = 0
-    #print( mMatrix.sum(0))
-
+    # Looping until they converge
     while not np.allclose(r, prevR, rtol= 1.e-3, atol=1.e-3):
         prevR = r
         r = (betaTimesStoch * prevR) + leapProp
@@ -60,15 +60,12 @@ def main():
     print("Converged Rank vector:")
     print(r)
 
-    print("Previous R:")
-    print(prevR)
+    #print("Previous R:")
+   # print(prevR)
 
     #Total iterations
 
     print("Total iterations", iterations)
-
-    print("Stochastic matrix: ")
-    print(stochasticArray)
 
 
 if __name__ == "__main__":
